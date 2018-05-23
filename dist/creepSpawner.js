4ad06010520d47a4d0b988cdb7cd20910846a1f1
Samuel Leclerc <sam.h.leclerc@gmail.com>
2018-05-22 21:35:01 -0400

    added rule in case no more creeps in room

--- dist/creepSpawner.js
+++ dist/creepSpawner.js
@@ -16,12 +16,12 @@ var creepSpawner =
             //TODO add logic for multiple spawner
             var spawnName = Game.rooms[room].find(FIND_MY_SPAWNS)[0].name;
 
-            if(rolesInRoom[c.ROLE.UPGRADER] < 10)
+            if(!(c.ROLE.UPGRADER in rolesInRoom) || rolesInRoom[c.ROLE.UPGRADER] < 10)
             {
                 upgrader.build(spawnName);
             }
             
-            if(rolesInRoom[c.ROLE.HARVESTER] < 5)
+            if(!(c.ROLE.HARVESTER in rolesInRoom) || rolesInRoom[c.ROLE.HARVESTER] < 5)
             {
                 harvester.build(spawnName);
             }
